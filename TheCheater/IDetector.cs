6e5df8327ef988e136f9cb69f66550eba6a22f3b
KorFresh <korfresh@naver.com>
2016-02-12 15:03:26 +0900

    ^^

---
+++ TheCheater/IDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using SharpDX;
+
+namespace TheCheater
+{
+    public enum DetectorSetting { Preferred, Safe, AntiHumanizer}
+    interface IDetector
+    {
+        void Initialize(Obj_AI_Hero hero, DetectorSetting setting = DetectorSetting.Safe);
+        void ApplySetting(DetectorSetting setting);
+        void FeedData(Vector3 targetPos);
+        int GetScriptDetections();
+        string GetName();
+    }
+}
